d0beda61f0ebad507d031fbdc5d316e4a2068eae
Art Abramov <mail@artabr.com>
2025-06-04 15:08:13 +0400

    feat: bootstrap i18n

--- src/pages/[[...pages]].tsx
+++ src/pages/[[...pages]].tsx
@@ -1,6 +1,11 @@
+import Code from '@/components/Code';
+import PageLayout from '@/components/PageLayout';
 import { RenderBuilderContent } from '@/components/builder';
 import { builder } from '@builder.io/react';
+import pick from 'lodash/pick';
 import type { GetStaticPropsContext, InferGetStaticPropsType } from 'next';
+import { useTranslations } from 'next-intl';
+import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
 // TODO: Check if it's safe to use the Builder API key here
@@ -12,6 +17,9 @@ export default function Pages({
   page,
   attributes,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const t = useTranslations('Index');
+  const { locale } = useRouter();
+
   const builderModelName = 'page';
 
   useEffect(() => {
@@ -20,12 +28,33 @@ export default function Pages({
 
   const { title, description } = page?.data || {};
   return (
-    <>
+    <PageLayout title={t('title')}>
+      <div>
+        {t.rich('description', {
+          locale: locale!,
+          p: (children) => <p>{children}</p>,
+          code: (children) => <Code>{children}</Code>,
+        })}
+      </div>
+      <ul>
+        {Pages.messages.map((componentName) => (
+          <li key={componentName} style={{ marginBottom: 5 }}>
+            <Code>{componentName}</Code>
+          </li>
+        ))}
+      </ul>
       {/* Render the Builder page */}
       <RenderBuilderContent content={page} model={builderModelName} />
-    </>
+    </PageLayout>
   );
 }
+
+// The namespaces can be generated based on used components. `PageLayout` in
+// turn requires messages for `Navigation` and therefore a recursive list of
+// namespaces is created dynamically, where the owner of a component doesn't
+// have to know which nested components are rendered. Note that this approach
+// is limited to components which are not lazy loaded.
+Pages.messages = ['Pages', ...PageLayout.messages];
 
 export async function getStaticProps({
   params,
@@ -59,6 +88,10 @@ export async function getStaticProps({
     props: {
       page,
       attributes: attributes,
+      messages: pick(
+        (await import(`../../messages/${locale}.json`)).default,
+        Pages.messages,
+      ),
     },
     revalidate: 1,
   };
